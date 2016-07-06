c511d2593d39f117057aff22a218cfa30731d5fd
miholeus <me@miholeus.com>
2016-07-06 18:53:59 +0300

    recent blog posts #3

---
+++ chapter3/blog_recent3.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+    getTitles(res);
+}).listen(8000, "127.0.0.1");
+
+function getTitles(res)
+{
+    fs.readFile('./titles.json', function(err, data){
+        if (err) return hadError(err);
+        getTemplate(JSON.parse(data.toString()), res);
+    });
+}
+
+function hadError(err)
+{
+    console.error(err);
+    res.end('Server error');
+}
+
+function getTemplate(titles, res)
+{
+    fs.readFile('./template.html', function(err, data){
+        if (err) return hadError(err);
+        formatHtml(titles, data.toString(), res);
+    });
+}
+
+function formatHtml(titles, tmpl, res)
+{
+    var html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end(html);
+}
