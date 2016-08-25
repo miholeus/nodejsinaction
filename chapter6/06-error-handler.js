b1beb8864ebb0fd3917e2f792371d0cfa6b2fd74
miholeus <me@miholeus.com>
2016-08-25 21:01:27 +0300

    chapter 7

--- chapter6/06-error-handler.js
+++ chapter6/06-error-handler.js
@@ -1,3 +1,22 @@
-/**
- * Created by miholeus on 20.08.16.
- */
+var connect = require('connect');
+
+function errorHandler() {
+    var env = process.env.NODE_ENV || 'development';
+    return function(err, req, res, next) {
+        res.statusCode = 500;
+        switch (env) {
+            case 'development':
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(err));
+                break;
+            default:
+                res.end("Server error");
+        }
+    }
+}
+connect()
+    .use(function(req, res){
+        foo();// error here
+})
+    .use(errorHandler())
+    .listen(3000);
---
+++ chapter7/01-cookie.js
@@ -0,0 +1,11 @@
+var connect = require('connect');
+var cookieParser = require('cookie-parser');
+
+var app = connect()
+    .use(cookieParser('tobi is a cool ferret'))
+    .use(function(req, res){
+        console.log(req.cookies);
+        console.log(req.signedCookies);
+        res.end("hello\n");
+    });
+app.listen(3000);
