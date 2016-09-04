aa0832791d554a7b396093fa39b0c42b1dbbd85a
miholeus <me@miholeus.com>
2016-09-04 13:49:36 +0300

    photos upload

--- chapter8/photo/app.js
+++ chapter8/photo/app.js
@@ -23,7 +23,7 @@ app.set('photos', path.join(__dirname, 'public/photos'));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- chapter8/photo/routes/photos.js
+++ chapter8/photo/routes/photos.js
@@ -1,27 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
+var formidable = require('formidable');
 
-var photos = [];
-photos.push({
-    name: 'Node.js Logo',
-    path: 'http://nodejs.org/images/logos/nodejs-green.png'
-});
-photos.push({
-    name: 'Ryan Speaking',
-    path: 'http://nodejs.org/images/ryan-speaker.jpg'
-});
+function isFormData(req) {
+    var type = req.headers['content-type'] || '';
+    return 0 == type.indexOf('multipart/form-data');
+}
 
-router.get('/', function(req, res){
-    res.render('photos', {
-        title: 'Photos',
-        photos: photos
-    })
+var Photo = require('../models/Photo');
+
+router.get('/', function(req, res, next){
+    Photo.find({}, function (err, photos){
+        if (err) return next(err);
+        res.render('photos', {
+            title: 'Photos',
+            photos: photos
+        })
+    });
 });
 
 router.get('/upload', function(req, res){
     res.render('photos/upload', {
         title: 'Photo upload'
     })
+});
+
+router.post('/upload', function(req, res, next){
+    var dir = req.app.get('photos');
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    if(!isFormData(req)) {
+        res.statusCode = 400;
+        res.end('Bad request: expecting multipart/form-data');
+        return;
+    }
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files){
+        // По умолчанию исходное имя файла
+        var img = files['photo[image]'];
+        var name = fields['photo[name]'] || img.name;
+        var path = join(dir, img.name);
+        // Переименование файла
+        fs.rename(img.path, path, function(err){
+            if (err) return next(err);
+            Photo.create({
+                name: name,
+                path: img.name
+            }, function(err){
+                // Делегируем ошибки
+                if (err) return next(err);
+                res.redirect('/');
+            });
+        });
+    });
 });
 
 module.exports = router;
